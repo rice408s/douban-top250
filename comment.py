119b56b3e2238e109b39c5eb6bbdbca208e3584c
rice408s <hongwei.wang408@gmail.com>
2024-04-19 14:28:48 +0800

    完成爬取豆瓣top250电影评论

--- comment.py
+++ comment.py
@@ -1,90 +1,91 @@
 import re
+import threading
 import requests
-from utools import Get_movie_url_list, headers
+from utools import Add_to_excel, Get_movie_url_list, headers
 from bs4 import BeautifulSoup  # type: ignore
 
 
 class Comment:
     def __init__(
         self,
-        title,
+        movie,
         movie_id,
-        username,
         user_id,
         comment_time,
         comment,
         rate,
     ):
-        self.title = title
-        self.username = username
+        self.movie = movie
         self.movie_id = movie_id
         self.user_id = user_id
         self.comment_time = comment_time
         self.comment = comment
         self.rate = rate
 
-    def __str__(self):
-        return (
-            "title: "
-            + self.title
-            + "\n"
-            + "movie_id: "
-            + self.movie_id
-            + "\n"
-            + "username: "
-            + self.username
-            + "\n"
-            + "user_id: "
-            + self.user_id
-            + "\n"
-            + "comment_time: "
-            + self.comment_time
-            + "\n"
-            + "comment: "
-            + self.comment
-            + "\n"
-            + "rate: "
-            + self.rate
-            + "\n"
-        )
-
 
 # 数据处理函数
 def data_is_null(data):
     if data:
-        return data[0]
+        return data[0].get_text()
     else:
         return ""
 
 
-def Get_comment_info(url, header, page_num, id,comment_list=[]):
-    # print("downloading:", url, "......\n")
+def Get_comment_info(url, header, page_num, id, comment_list=[]):
+    url = url + "/comments?start=" + str(page_num) + "&limit=20"
+    # print(url)
     res = requests.get(url=url, headers=header)
     context = res.text
-    pattern='<span property="v:itemreviewed">(.*?)</span>'
-    title=re.findall(pattern, context, re.S)[0]
-    movie_id=id
-    url = url + "/reviews?start=" + str(page_num)
-    res = requests.get(url=url, headers=header)
-    context = res.text
+    # print(context)
     soup = BeautifulSoup(context, "html.parser")
-    comments = soup.findAll("div", class_="main review-item")
-    # for comment in comments:
-    print(comments[0])
-    pattern='''<a class="avator" href="https://www.douban.com/people/bighead/">
-<img height="24" src="(.*?)" width="24"/>
-</a>'''
-    re.findall(pattern, str(comments[0]), re.S)
+    # print(soup)
+    movie = data_is_null(soup.find_all("title")).split(" ")[0].strip()
+    # print(movie)
+    comments = soup.find_all("div", class_="comment-item")
+    for x in comments:
+        # print(x)
+        uid = x.find("img", class_="").get("src").split("/")[-1].split("-")[0][1:]
+        content = x.find("span", class_="short").get_text().strip()
+        # print(uid)
+        # print(content)
+        pattern = '<span class="allstar(.*?) rating"'
+        if re.findall(pattern, str(x), re.S):
+            rate = int(re.findall(pattern, str(x), re.S)[0]) / 10
+        else:
+            rate = 0
+        # print(rate)
+        comment_time = x.find("span", class_="comment-time").get_text().strip()
+        # print(comment_time)
+        C = Comment(movie, id, uid, comment_time, content, rate)
+        comment_list.append(C)
+
+
+def Get_comment_info_in_thread(url, headers, id, comment_list=[]):
+    print("downloading:", url, "......\n")
+    for i in range(1, 11):
+        Get_comment_info(url, headers, (i - 1) * 20, id, comment_list)
+
 
 def Spider():
-    # url_list = Get_movie_url_list()
-    u1 = "https://movie.douban.com/subject/1292052"
-    # 先获取单部电影的评论
+    url_list = Get_movie_url_list()
+    
     comment_list = []
-    # for i in range(1, 11):
-    #     Get_comment_info(u1, headers, (i - 1) * 20, comment_list)
-    Get_comment_info(u1, headers, 0,2,comment_list)
+    Thread_list = []
+    for i in range(0, len(url_list)):
+        t = threading.Thread(
+            target=Get_comment_info_in_thread,
+            args=(url_list[i], headers, i + 2, comment_list),
+        )
 
+        t.start()
+        Thread_list.append(t)
+        
+    for t in Thread_list:
+        t.join()
+        
+    comment_list.sort(key=lambda x: x.movie_id)
+    Add_to_excel(comment_list, "comment.xlsx")
+    
 
 if __name__ == "__main__":
     Spider()
--- utools.py
+++ utools.py
@@ -8,14 +8,18 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36"
 }
 
+
 # 获取每一页的url
 def Get_urls_by_page(page_num, url, header, page_url_list=[]):
     res = requests.get(url=url + "?start=" + (str((page_num - 1) * 25)), headers=header)
     context = res.text
+    # print(context)
     pattern = "<li>(.*?)</li>"
     res = re.findall(pattern, context, re.S)
+
     pattern = '<a href="(.*?)/">'
-    for i in range(0, 25):
+    print(len(res))
+    for i in range(0, len(res)):
         url = re.findall(pattern, res[i], re.S)
         page_url_list.append(url[0])
 
@@ -35,10 +39,13 @@ def Add_to_excel(data, name):
     pd.set_option("display.max_rows", None)
     # 设置value的显示长度为100，默认为50
     pd.set_option("max_colwidth", 100)
-
+    print(data[0])
     variables = list(data[0].__dict__.keys())
     df = pd.DataFrame(
         [[getattr(i, j) for j in variables] for i in data], columns=variables
     )
 
     df.to_excel(name, index=False)
+
+
+
